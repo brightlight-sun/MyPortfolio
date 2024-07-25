1962aa76ba1922a0b371dadd7d293fc2efb7f7ee
Deepak Namdev <95995520+brightlight-sun@users.noreply.github.com>
2024-07-25 23:53:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// type js content
+var typed = new Typed('#element',
+    {
+        strings: ['Hi, I am a 3rd year BE CSE Student.',' Hi, I am a Web Developer.','Hi, I am a 3rd year BE CSE Student.' ],
+        typeSpeed: 50
+    });
